d9616abe7f2f619c0053c61ba2e987425a44282e
armenasryan04 <armenasryan04@gmail.com>
2024-12-26 13:33:17 +0400

    Initial commit

---
+++ src/main/java/com/aisgoogle/drive/googledriveais/GoogleDriveAisApplication.java
@@ -0,0 +1,13 @@
+package com.aisgoogle.drive.googledriveais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveAisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDriveAisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aisgoogle/drive/googledriveais/GoogleDriveAisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisgoogle.drive.googledriveais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveAisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
